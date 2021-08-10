f88f3cf6a293f0d05a876b7c272505cbe59ea2c6
Blond1nka <po.heize@gmail.com>
2021-08-10 10:18:04 +0300

    add athletController
    athletController, startup, constructor

---
+++ Olympics/Olympics/Controllers/AthleteController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Olympics.Controllers
+{
+    public class AthleteController : Controller
+    {
+        // GET: AthleteController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AthleteController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AthleteController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AthleteController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AthleteController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AthleteController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AthleteController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AthleteController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Olympics/Olympics/Services/AthleteDBService.cs
+++ Olympics/Olympics/Services/AthleteDBService.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Olympics.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,37 @@ namespace Olympics.Services
 {
     public class AthleteDBService
     {
-        
+        private SqlConnection _connection;
+        public AthleteDBService(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public List<AthleteModel> AllAthletes()
+        {
+            _connection.Open();
+
+            List<AthleteModel> athletes = new();
+
+            using var command = new SqlCommand("SELECT * FROM Athlete;", _connection);
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                AthleteModel athlete = new()
+                {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Surname = reader.GetString(2),
+                    CountryId = reader.GetInt32(3)
+                };
+
+                athletes.Add(athlete);
+            }
+
+            _connection.Close();
+
+            return athletes;
+        }
     }
 }
